fd9aebbc70e9a1a1c1ce155721b76ceceaa85138
kashiwade <music5d7_1415@outlook.com>
2022-02-23 14:44:16 +0900

    fix

--- src/ProjectZone/ProjectZone.tsx
+++ src/ProjectZone/ProjectZone.tsx
@@ -1,38 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { Collapse } from "@mui/material";
+import React from "react";
 import Box from "@mui/material/Box";
-import styled from "styled-components";
 import BasicTabs from "../components/BasicTabs";
-import BasicTabsNoHidden from "../components/BasicTabsNoHidden";
-import { Button } from "@mui/material";
-import * as initialObj from "../module/initialObj";
-import FolderList from "../components/FolderList";
-import { width } from "@mui/system";
-import getAPI from "../module/getAPI";
-import TopToolBarAria from "../TopToolBarAria/TopToolBarAria";
-import FileList from "../components/FileList";
-import { useSelector, useDispatch } from "react-redux";
-
-import {
-  changeShouldShowDirPathInProjectZone,
-  addOpeningDirInTree,
-  deleteOpeningDirInTree,
-  selectShouldShowDirPathInProjectZone,
-  selectOpeningDirInTree,
-} from "../redux/slice/SelectedFolderStatsSlice";
+import Files from "./components/Files";
 
 const ProjectZone = () => {
-  const ShouldShowDirPathInProjectZone = useSelector(
-    selectShouldShowDirPathInProjectZone
-  );
-  const [mainWindowStats, setMainWindowStats] = useState(
-    initialObj.mainWindowStats
-  );
-
   return (
     <BasicTabs labels={["Files", "DAW View", "Spectrogram"]}>
       <div>
-        <FileList dirPath={ShouldShowDirPathInProjectZone} />
+        <Files />
       </div>
       <Box
         sx={{
